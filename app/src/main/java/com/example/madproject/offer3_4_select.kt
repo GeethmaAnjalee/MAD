e14a2184648b34106cba25438c7eb66bcc0e79f3
GeethmaAnjalee <githuanjalee@gmail.com>
2023-05-06 14:33:10 +0530

    project prosesing.

---
+++ app/src/main/java/com/example/madproject/offer3_4_select.kt
@@ -0,0 +1,11 @@
+package com.example.madproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class offer3_4_select : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offer34_select)
+    }
+}
